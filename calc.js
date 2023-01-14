8c82c2f25077bb3e175cc56cea4ab4160175f2da
Gwan <slavik.art15@gmail.com>
2023-01-14 18:44:01 +0300

    add all files

---
+++ calc.js
@@ -0,0 +1,40 @@
+const display = document.querySelector('#display');
+const buttons = document.querySelectorAll('button');
+
+buttons.forEach((item) => {
+    item.onclick = () => {
+        if(item.id === 'clear') {
+            display.innerText = '';
+        } else if(item.id === 'back') {
+            let string = display.innerText.toString();
+            display.innerText = string.substr(0, string.length-1);
+        } else if(display.innerText !== '' && item.id === 'equal') {
+            display.innerText = eval(display.innerText);
+        } else if(item.id === '√') {
+            display.innerText = Math.sqrt(display.innerText);
+        } else if(item.id === 'plusm') {
+            if(display.innerText === ''){
+                display.innerText = '-';
+            } else if(display.innerText === '-'){
+                display.innerText = '';
+            } 
+        } else if(display.innerText === '' && item.id ==='equal') {
+            display.innerText = 'Empty';
+            setTimeout(() => (display.innerText = ''), 2000);
+        } else {
+            display.innerText += item.id;
+        }
+    }
+})
+
+const themeToggleBtn = document.querySelector('.theme-toggler');
+const calculator = document.querySelector('.calculator');
+const toggleIcon = document.querySelector('.toggle-icon');
+
+let isDark = true;
+themeToggleBtn.onclick = () => {
+    calculator.classList.toggle('dark');
+    themeToggleBtn.classList.toggle('active');
+    isDark = !isDark;
+}
+
